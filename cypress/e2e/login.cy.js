72f9b375cc8ffec1d48c9fd6562bb8935444292d
Reeyyyh <rhnlnv@gmail.com>
2026-06-05 11:03:58 +0700

    setup Cypress for auth testing

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,14 @@
+describe('Login Feature - Structura', () => {
+
+  it('TC-LOG-00 - Login template test', () => {
+
+    // TODO: implement login test case
+
+    cy.visit('http://localhost:8000/login')
+
+    // Placeholder assertion
+    cy.get('form').should('exist')
+
+  })
+
+})
---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,14 @@
+describe('Register Feature - Structura', () => {
+
+  it('TC-REG-00 - Register template test', () => {
+
+    // TODO: implement register test case
+
+    cy.visit('http://localhost:8000/register')
+
+    // Placeholder assertion
+    cy.get('form').should('exist')
+
+  })
+
+})
